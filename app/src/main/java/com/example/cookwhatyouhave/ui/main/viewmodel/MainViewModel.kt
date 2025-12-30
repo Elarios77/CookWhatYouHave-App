790eb90beebd657e69b9dd198b3065349a542036
Lazaros <tipk1926@gmail.com>
2025-12-30 12:23:35 +0200

    small fixes

--- app/src/main/java/com/example/cookwhatyouhave/ui/main/viewmodel/MainViewModel.kt
+++ app/src/main/java/com/example/cookwhatyouhave/ui/main/viewmodel/MainViewModel.kt
@@ -4,6 +4,7 @@ import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.example.cookwhatyouhave.usecase.GetMealsByIngredientUseCase
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.async
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.flow.asStateFlow
@@ -14,45 +15,80 @@ import javax.inject.Inject
 @HiltViewModel
 class MainViewModel @Inject constructor(
     private val getMealsByIngredientUseCase: GetMealsByIngredientUseCase
-): ViewModel() {
+) : ViewModel() {
     private val _uiState = MutableStateFlow(MainUiState())
     val uiState: StateFlow<MainUiState> = _uiState.asStateFlow()
-    fun onSearchChange(text:String){
-        _uiState.update { it.copy(
-            searchQuery = text.trim(),
-            searchPerformed = false
-        ) }
+    fun onSearchChange(text: String) {
+        _uiState.update {
+            it.copy(
+                searchQuery = text,
+                searchPerformed = false
+            )
+        }
 
-        if(text.isBlank()){
-            _uiState.update { it.copy(searchResults = emptyList()) }
+        if (text.isBlank()) {
+            _uiState.update {
+                it.copy(
+                    searchResults = emptyList(),
+                    searchPerformed = false
+                )
+            }
         }
     }
 
-    fun searchRecipes(){
+    fun searchRecipes() {
         val query = _uiState.value.searchQuery
-        if(query.isBlank()) return
-        val formattedQuery = if(query.endsWith("s",ignoreCase = true)){
-            query.dropLast(1)
-        }else{
-            query
-        }
-         viewModelScope.launch {
-             _uiState.update { it.copy(isLoading = true, error = null) }
 
-             getMealsByIngredientUseCase(formattedQuery)
-             .onSuccess { meals->
-                 _uiState.update { it.copy(
-                     isLoading = false,
-                     searchResults = meals,
-                     searchPerformed = true
-                 ) }
-             }.onFailure { error->
-                 _uiState.update { it.copy(
-                     isLoading = false,
-                     error = error.message,
-                     searchPerformed = true
-                 ) }
-             }
-         }
+        if(query.isBlank()){
+            _uiState.update { it.copy(
+                searchResults =  emptyList(),
+                searchPerformed = false
+            ) }
+            return
+        }
+
+        val singularQuery: String
+        val pluralQuery: String
+
+        if (query.endsWith("s", ignoreCase = true)) {
+            singularQuery = query.dropLast(1)
+            pluralQuery = query
+        } else {
+            singularQuery = query
+            pluralQuery = query + "s"
+        }
+        viewModelScope.launch {
+            _uiState.update { it.copy(isLoading = true, error = null) }
+
+            val qSingular = async { getMealsByIngredientUseCase(singularQuery) }
+            val qPlural = async { getMealsByIngredientUseCase(pluralQuery) }
+
+            val resultSingular = qSingular.await()
+            val resultPlural = qPlural.await()
+
+            val list1 = resultSingular.getOrNull() ?: emptyList()
+            val list2 = resultPlural.getOrNull() ?: emptyList()
+
+            val resultList = (list1 + list2).distinctBy { it.id }
+
+            if (resultList.isNotEmpty()) {
+                _uiState.update {
+                    it.copy(
+                        isLoading = false,
+                        searchResults = resultList,
+                        searchPerformed = true
+                    )
+                }
+            } else {
+                _uiState.update {
+                    it.copy(
+                        isLoading = false,
+                        searchResults = emptyList(),
+                        searchPerformed = true,
+                        error = resultSingular.exceptionOrNull()?.message
+                    )
+                }
+            }
+        }
     }
 }
